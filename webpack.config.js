b9c0eb092b84dfb96611fd4edf98ce71039f4610
Chris Woods <chris.woods@spr.com>
2018-05-25 09:53:52 -0500

    Test

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+import path from 'path';
+
+export default {
+    entry: path.resolve(__dirname, 'src/index'),
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }  
+        ]
+    }
+}
