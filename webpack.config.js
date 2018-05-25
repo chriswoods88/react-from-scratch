8f016c7df2ea0ecbc3c15128bd20419329c0e87d
Chris Woods <chris.woods@spr.com>
2018-05-25 10:40:39 -0500

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ export default {
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }  
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
     }
 }
